bcf0df8462c9924de6a93a034af089707635adc4
DruSadeMumba <drusademumba@gmail.com>
2022-05-14 13:01:26 +0300

    Add methods to the user interface

--- src/main/java/models/Dao/Interfaces/InterfaceUser.java
+++ src/main/java/models/Dao/Interfaces/InterfaceUser.java
@@ -1,5 +1,22 @@
 package models.Dao.Interfaces;
 
+import models.User;
+
+import java.util.List;
+
 public interface InterfaceUser {
 
+        //Implement CRUD
+
+    //Create
+    void add (User user);
+
+    //Read
+    List<User> getAll();
+
+    //Update
+    void update (int id, String name, String position, String role, String departmentId);
+
+    //Delete
+    void deleteById(int id);
 }
