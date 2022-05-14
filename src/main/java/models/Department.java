8fc5b5340c2eae11d13db29d9d5fd284697e8aec
DruSadeMumba <drusademumba@gmail.com>
2022-05-14 17:22:15 +0300

    Add variables, constructor and methods to department.java

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,25 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+    private int id;
+    private String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
