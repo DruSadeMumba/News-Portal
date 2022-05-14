8d04b7476ca1647573ba18b284d9bf284d778705
DruSadeMumba <drusademumba@gmail.com>
2022-05-14 17:49:11 +0300

    Add methods to Department interface

--- src/main/java/models/Dao/Interfaces/InterfaceDepartment.java
+++ src/main/java/models/Dao/Interfaces/InterfaceDepartment.java
@@ -1,4 +1,26 @@
 package models.Dao.Interfaces;
 
+import models.Department;
+
+
+import java.util.List;
+
 public interface InterfaceDepartment {
+
+            //Implement CRUD
+    
+    //Create
+    void add (Department department);
+
+    //Read
+    List<Department> getAll();
+
+    //Update
+    void update (int id, String name);
+
+    //Delete
+    void deleteById(int id);
+    void clearAll();
+
+    Department findById(int id);
 }
