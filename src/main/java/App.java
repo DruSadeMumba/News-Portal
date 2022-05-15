1f165dd6f92321d5a32752d2c424001d859df0aa
DruSadeMumba <drusademumba@gmail.com>
2022-05-15 16:55:29 +0300

    Create paths to add and view departments

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
 import com.google.gson.Gson;
+import models.Dao.Sql2o.SqlDepartment;
 import models.Dao.Sql2o.SqlUser;
+import models.Department;
+import models.DepartmentPost;
 import models.User;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -12,10 +15,9 @@ public class App {
         String connectionString = "jdbc:postgresql://localhost:5432/news_portal_test";
         Sql2o sql2o = new Sql2o(connectionString, null, null);
 
-        Connection conn;
-        conn = sql2o.open();
         Gson gson = new Gson();
         SqlUser user = new SqlUser(sql2o);
+        SqlDepartment department = new SqlDepartment(sql2o);
 
                 // get all users
         get("/users", "application/json", (req, res)->{
@@ -33,6 +35,23 @@ public class App {
             user.add(newUser);
             res.status(201);
             return gson.toJson(newUser);
+        });
+
+                //get all departments
+        get("/departments", "application/json", (req, res)->{
+            if (department.getAll().size() > 0) {
+                return gson.toJson(department.getAll());
+            }
+            else {
+                return "{\"message\":\"I'm sorry, but no departments are currently listed in the database.\"}";
+            }
+        });
+
+        post("/departments/new", "application/json", (req, res)->{
+            Department newDepartment = gson.fromJson(req.body(), Department.class);
+            department.add(newDepartment);
+            res.status(201);
+            return gson.toJson(newDepartment);
         });
 
     }
